5ef30569d68b451dac7c2556262134c59e47eec5
Nelson Neto <nelson16.code@gmail.com>
2023-10-07 09:33:03 -0400

    feat: update dono movimentation

--- js/game.js
+++ js/game.js
@@ -5,6 +5,12 @@
   const WIDTH = 1024;
   const PROB_CLOUD = 100;
 
+  const DINO_STATUS_STOPPED = 0;
+  const DINO_STATUS_RUNNING = 1;
+  const DINO_STATUS_JUMPING = 2;
+  const DINO_STATUS_SQUATTING = 3;
+  const DINO_STATUS_DIED = 4;
+
   let gameLoop;
   let generateCloudsLoop;
   let desert;
@@ -16,12 +22,39 @@
     generateCloudsLoop = setInterval(generateClouds, 1500);
     desert = new Desert();
     dino = new Dino();
-    utils = new Utils();
   }
 
   window.addEventListener("keydown", (e) => {
-    if (e.code === "Space") {
-      if (dino.status === 0) dino.status = 1;
+    if (e.code === "Space" || e.code === "ArrowUp") {
+      dino.up = true;
+
+      dino.jump();
+    }
+
+    if (e.code === "ArrowDown") {
+      dino.down = true;
+
+      if (dino.status == DINO_STATUS_RUNNING) {
+        dino.status = DINO_STATUS_SQUATTING;
+        dino.element.style.backgroundPositionX = this.backgroundPositionsX.squartting1
+        dino.element.className = "dino_squart";
+      }
+    }
+  })
+
+  window.addEventListener("keyup", (e) => {
+    if (e.code === "Space" || e.code === "ArrowUp") {
+      dino.up = false;
+    }
+
+    if (e.code === "ArrowDown") {
+      dino.down = false;
+
+      if (dino.status == DINO_STATUS_SQUATTING) {
+        dino.status = DINO_STATUS_RUNNING;
+        dino.element.style.backgroundPositionX = this.backgroundPositionsX.running1;
+        dino.element.className = "dino";
+      }
     }
   })
 
@@ -49,11 +82,15 @@
       this.backgroundPositionsX = {
         running1: "-1391px",
         running2: "-1457px",
+        squartting1: "-1655px",
+        squartting2: "-1740px",
         jumping: "-1259px"
       }
-      this.#status = 0; // 0-correndo, 1-subindo, 2-descendo
+      this.#status = DINO_STATUS_RUNNING;
       this.minHeight = 2;
-      this.maxHeight = 100;
+      this.maxHeight = 150;
+      this.up = false;
+      this.down = false;
       this.element = document.createElement("div")
       this.element.className = "dino";
       this.element.style.backgroundPositionX = this.backgroundPositionsX.running1;
@@ -65,21 +102,102 @@
      * @param {number} value
      */
     set status(value) {
-      if (value >= 0 && value <= 2) this.#status = value;
+      if (value >= 0 && value <= 4) this.#status = value;
     }
     get status() {
       return this.#status;
     }
+
     run() {
-      if (this.#status === 0 && frame % 20 === 0) this.element.style.backgroundPositionX = this.element.style.backgroundPositionX === this.backgroundPositionsX.running1 ? this.backgroundPositionsX.running2 : this.backgroundPositionsX.running1;
-      else if (this.#status === 1) {
-        this.element.style.backgroundPositionX = this.backgroundPositionsX.jumping;
-        this.element.style.bottom = `${parseInt(this.element.style.bottom) + 1}px`;
-        if (parseInt(this.element.style.bottom) >= this.maxHeight) this.status = 2;
+      if (this.#status == DINO_STATUS_RUNNING || this.#status == DINO_STATUS_JUMPING) {
+        if (this.element.style.backgroundPositionX == this.backgroundPositionsX.squartting1
+          || this.element.style.backgroundPositionX == this.backgroundPositionsX.squartting2) {
+          this.element.style.backgroundPositionX = this.backgroundPositionsX.running1
+        }
+
+        this.element.className = "dino";
       }
-      else if (this.#status === 2) {
-        this.element.style.bottom = `${parseInt(this.element.style.bottom) - 1}px`;
-        if (parseInt(this.element.style.bottom) <= this.minHeight) this.status = 0;
+      if (this.#status == DINO_STATUS_SQUATTING) {
+        if (this.element.style.backgroundPositionX == this.backgroundPositionsX.running1
+          || this.element.style.backgroundPositionX == this.backgroundPositionsX.running2) {
+          this.element.style.backgroundPositionX = this.backgroundPositionsX.squartting1
+        }
+
+        this.element.className = "dino_squart";
+      }
+
+      if (this.#status == DINO_STATUS_RUNNING && frame % 20 === 0) {
+        this.element.style.backgroundPositionX = this.element.style.backgroundPositionX === this.backgroundPositionsX.running1 ? this.backgroundPositionsX.running2 : this.backgroundPositionsX.running1;
+      }
+      if (this.#status == DINO_STATUS_SQUATTING && frame % 20 === 0) {
+        this.element.style.backgroundPositionX = this.element.style.backgroundPositionX === this.backgroundPositionsX.squartting1 ? this.backgroundPositionsX.squartting2 : this.backgroundPositionsX.squartting1;
+      }
+    }
+
+    async jump() {
+      if (dino.status == DINO_STATUS_RUNNING) {
+        dino.status = DINO_STATUS_JUMPING;
+
+        this.element.style.bottom = `${this.minHeight + 1}px`;
+
+        this.element.style.backgroundPositionX = this.backgroundPositionsX.jumping;
+
+        let percentShortJump = 0.65;
+
+        let percentMidleJump = 0.85;
+
+        let targetHeight = this.maxHeight * percentShortJump;
+
+        let speedpx = 3;
+
+        //up
+        while (parseInt(this.element.style.bottom) <= targetHeight && this.down == false) {
+          if (parseInt(this.element.style.bottom) >= this.maxHeight * percentMidleJump) {
+            speedpx = 1;
+          }
+          else if (parseInt(this.element.style.bottom) >= this.maxHeight * percentShortJump) {
+            speedpx = 2;
+          }
+          else {
+            speedpx = 3;
+          }
+
+          this.element.style.bottom = `${parseInt(this.element.style.bottom) + speedpx}px`;
+
+          if (targetHeight != this.maxHeight
+            && (targetHeight - 5 <= parseInt(this.element.style.bottom))
+            && this.up == true) {
+            targetHeight = this.maxHeight;
+          }
+
+          await new Promise(resolve => setTimeout(resolve));
+        }
+
+        //down
+        while (parseInt(this.element.style.bottom) >= this.minHeight) {
+          await new Promise(resolve => setTimeout(resolve));
+
+          if (parseInt(this.element.style.bottom) >= this.maxHeight * percentMidleJump) {
+            speedpx = 1;
+          }
+          else if (parseInt(this.element.style.bottom) >= this.maxHeight * percentShortJump) {
+            speedpx = 2;
+          }
+          else {
+            speedpx = 3;
+          }
+
+          this.element.style.bottom = `${parseInt(this.element.style.bottom) - (this.down ? 5 : speedpx)}px`;
+        }
+
+        if (this.down) {
+          this.status = DINO_STATUS_SQUATTING;
+          this.element.style.backgroundPositionX = this.backgroundPositionsX.squartting1
+          this.element.className = "dino_squart";
+        }
+        else {
+          this.status = DINO_STATUS_RUNNING;
+        }
       }
     }
   }
@@ -94,21 +212,13 @@
     }
     move(cloud) {
       if (frame % 2 === 0) {
-        if (utils.pxToInt(cloud.element.style.right) < WIDTH) {
+        if (parseInt(cloud.element.style.right) < WIDTH) {
           cloud.element.style.right = `${parseInt(cloud.element.style.right) + 1}px`;
         }
         else {
           cloud.element.remove();
         }
       }
-    }
-  }
-
-  class Utils {
-    constructor() {}
-
-    pxToInt(px) {
-      return parseInt(px.substring(-2));
     }
   }
 
